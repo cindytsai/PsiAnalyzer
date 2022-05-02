4e032390063d53fce383fa005c03920a527579af
cindytsai <6188366+cindytsai@users.noreply.github.com>
2022-05-02 14:33:47 +0800

    Update GetVelocity.

--- PsiAnalyzer/GetVelocity.py
+++ PsiAnalyzer/GetVelocity.py
@@ -3,16 +3,31 @@ import matplotlib.pyplot as plt
 import Gradient
 
 
-def GetVelocity(dataRe, dataIm, cell_space, fft_pad, check_convergence=False):
+def GetVelocity(dataRe, dataIm, cell_space, fft_pad, check_convergence=False, check_pad=[]):
+    """
+    Get velocity field of psi.
+
+    :param dataRe: Re( psi )
+    :param dataIm: Im( psi )
+    :param cell_space: Spacing between sample points.
+    :param fft_pad: Ratio of padding width to target field width at each side.
+    :param check_convergence: Check convergence. Default is False.
+    :param check_pad: List of padding ratio to check for convergence. If fft_pad does not include in the list, it will
+    be checked as well. This is only for convergence check, the output will still be evaluated using fft_pad.
+    :return: List [v_x, v_y, v_z]
+    """
     # get grad(dataRe) and grad(dataIm) through fft method
-    # plot difference of paddings from 1~fft_pad, if check_convergence == True.
-    # TODO: See if padding ratio alters from 0~fft_pad, will result converges. And plot diff from each paddings.
+    # plot difference of paddings in list check_pad, if check_convergence == True.
     if check_convergence is True:
         diff_Re = [[], [], []]  # difference average of previous padding to this padding in x,y,z respectively.
         diff_Im = [[], [], []]
-        flag = True
+        flag, first_enter = True, True
         num_samples = dataRe.shape[0] * dataRe.shape[1] * dataRe.shape[0]
-        for pad in range(fft_pad + 1):
+        if fft_pad not in check_pad:
+            check_pad.append(fft_pad)
+        check_pad.sort()
+        for pad in check_pad:
+            # save result in data1/2 respectively, so that we can compare.
             if flag is True:
                 dataRe1 = Gradient.Gradient_with_fft(dataRe, cell_space, pad)
                 dataIm1 = Gradient.Gradient_with_fft(dataIm, cell_space, pad)
@@ -21,20 +36,23 @@ def GetVelocity(dataRe, dataIm, cell_space, fft_pad, check_convergence=False):
                 dataRe2 = Gradient.Gradient_with_fft(dataRe, cell_space, pad)
                 dataIm2 = Gradient.Gradient_with_fft(dataIm, cell_space, pad)
                 flag = True
-            if pad != 0:
+            if first_enter is False:
                 for d in range(3):
                     diff_Re[d].append(np.sum(np.absolute(dataRe1[d] - dataRe2[d])) / num_samples)
                     diff_Im[d].append(np.sum(np.absolute(dataIm1[d] - dataIm2[d])) / num_samples)
+            first_enter = False
+
+            # load fft_pad result inside grad_dataRe/Im_x/y/z, this is the padding evaluated for the output.
+            if pad == fft_pad:
+                if flag is False:
+                    grad_dataRe_x, grad_dataRe_y, grad_dataRe_z = dataRe1
+                    grad_dataIm_x, grad_dataIm_y, grad_dataIm_z = dataIm1
+                else:
+                    grad_dataRe_x, grad_dataRe_y, grad_dataRe_z = dataRe2
+                    grad_dataIm_x, grad_dataIm_y, grad_dataIm_z = dataIm2
 
         # TODO:START HERE. Plot the result.
-
-        # load grad_dataRe_x/y/z, and grad_dataIm_x/y/z
-        if flag is False:
-            grad_dataRe_x, grad_dataRe_y, grad_dataRe_z = dataRe1
-            grad_dataIm_x, grad_dataIm_y, grad_dataIm_z = dataIm1
-        else:
-            grad_dataRe_x, grad_dataRe_y, grad_dataRe_z = dataRe2
-            grad_dataIm_x, grad_dataIm_y, grad_dataIm_z = dataIm2
+        
 
     else:
         grad_dataRe_x, grad_dataRe_y, grad_dataRe_z = Gradient.Gradient_with_fft(dataRe, cell_space, fft_pad)
