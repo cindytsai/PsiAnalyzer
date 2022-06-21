99694ee3a0f091a4a4cf92df11e28e078c6bc8f1
cindytsai <6188366+cindytsai@users.noreply.github.com>
2022-06-21 22:05:41 +0800

    plot 3d vector field.

--- Plot/vector_plot.py
+++ Plot/vector_plot.py
@@ -11,8 +11,17 @@ field_shape = VelRota["x"].shape
 
 X, Y, Z = np.meshgrid(np.arange(field_shape[0]), np.arange(field_shape[1]), np.arange(field_shape[2]), indexing='ij')
 
-ax.quiver(X, Y, Z, VelRota["x"], VelRota["y"], VelRota["z"], length=0.1)
+#ax.quiver(X[0:25, 0:25, 0:25], Y[0:25, 0:25, 0:25], Z[0:25, 0:25, 0:25], 
+#           1e20*VelRota["x"][0:25, 0:25, 0:25],  1e20*VelRota["y"][0:25, 0:25, 0:25], 1e20*VelRota["z"][0:25, 0:25, 0:25], length=1)
+#plt.show()
+
+#ax.quiver(X[0:15, 0:15, 0:15], Y[0:15, 0:15, 0:15], Z[0:15, 0:15, 0:15], 
+#        1e21*VelRota["x"][0:15, 0:15, 0:15],  1e21*VelRota["y"][0:15, 0:15, 0:15], 1e21*VelRota["z"][0:15, 0:15, 0:15], length=0.8, normalize=True, cmap='jet', lw=0.5)
+#plt.show()
+
+
+ax.quiver(X[0:15, 0:15, 0:15], Y[0:15, 0:15, 0:15], Z[0:15, 0:15, 0:15], 
+        1e21*VelLong["x"][0:15, 0:15, 0:15],  1e21*VelLong["y"][0:15, 0:15, 0:15], 1e21*VelLong["z"][0:15, 0:15, 0:15], length=0.8, normalize=True, cmap='jet', lw=0.5)
 plt.show()
 
-ax.quiver(X, Y, Z, VelLong["x"], VelLong["y"], VelLong["z"], length=0.1)
-plt.show()
+
