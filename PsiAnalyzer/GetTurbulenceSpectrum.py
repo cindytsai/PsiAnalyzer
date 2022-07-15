5f74712affe8db14accefa7e9adaab42929d7db1
cindytsai <6188366+cindytsai@users.noreply.github.com>
2022-07-15 13:59:14 +0800

    Using linear k bin.

--- PsiAnalyzer/GetTurbulenceSpectrum.py
+++ PsiAnalyzer/GetTurbulenceSpectrum.py
@@ -15,7 +15,8 @@ def _pad_zeros(f, pad):
     return f_pad
 
 
-def GetTurbulenceSpectrum(v_x, v_y, v_z, cell_space, fft_pad, num_bin=200, check_convergence=False, check_pad=[],
+def GetTurbulenceSpectrum(v_x, v_y, v_z, cell_space, fft_pad, num_bin=200, k_bin_log_scale=False,
+                          check_convergence=False, check_pad=[],
                           filename="TurbulenceSpectrum.png", txt_basename="TurSpectrum_txt"):
     """
     Get turbulence spectrum logE - logk.
@@ -54,9 +55,14 @@ def GetTurbulenceSpectrum(v_x, v_y, v_z, cell_space, fft_pad, num_bin=200, check
         xx, yy, zz = np.meshgrid(kx2, ky2, kz2, indexing='ij')
         kk_norm = np.sqrt(xx + yy + zz)
 
+        # create k_bin be
         k_min, k_max = np.sqrt(kx2[1]), (1.0 + 1e-6) * np.sqrt(np.max(kx2) + np.max(ky2) + np.max(kz2))
-        k_norm = np.logspace(np.log10(k_min), np.log10(k_max), num=num_bin+1, endpoint=True)
-        k_shift = 10 ** ((np.log10(k_max) - np.log10(k_min)) / (num_bin * 2.0))
+        if k_bin_log_scale is True:
+            k_norm = np.logspace(np.log10(k_min), np.log10(k_max), num=num_bin+1, endpoint=True)
+            k_shift = 10 ** ((np.log10(k_max) - np.log10(k_min)) / (num_bin * 2.0))
+        else:
+            k_norm = np.linspace(k_min, k_max, num=num_bin+1, endpoint=True)
+            k_shift = (k_max - k_min) / (num_bin * 2.0)
         del kx2
         del ky2
         del kz2
@@ -72,11 +78,18 @@ def GetTurbulenceSpectrum(v_x, v_y, v_z, cell_space, fft_pad, num_bin=200, check
                 power_spectrum.append(np.sum(mask * v_k) / np.sum(mask))
 
         # add to image buffer, and shift k_norm.
-        plt.plot(k_norm[:-1] * k_shift, power_spectrum, '.-', label='pad ratio = {}'.format(pad))
+        if k_bin_log_scale is True:
+            k_bin = k_norm[:-1] * k_shift
+        else:
+            k_bin = k_norm[:-1] + k_shift
+        plt.plot(k_bin, power_spectrum, '.-', label='pad ratio = {}'.format(pad))
 
         # write to file
-        txt_name = "{}_pad={}.txt".format(txt_basename, pad)
-        np.savetxt(txt_name, np.stack((k_norm[:-1] * k_shift, np.asarray(power_spectrum)), axis=1), header="k\tE")
+        if k_bin_log_scale is True:
+            txt_name = "{}_pad={}_k_logscale.txt".format(txt_basename, pad)
+        else:
+            txt_name = "{}_pad={}_k_linscale.txt".format(txt_basename, pad)
+        np.savetxt(txt_name, np.stack((k_bin, np.asarray(power_spectrum)), axis=1), header="k\tE")
 
     # Plot the result
     plt.xscale('log')
